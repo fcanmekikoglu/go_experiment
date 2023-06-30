4f18adb17855b3f4fc527ff7ef6369d56fbe1068
F. Can Mekikoğlu <fcanmekikoglu@gmail.com>
2023-06-30 14:41:39 +0300

    working with modules

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fcanmekikoglu/go_experiment
+
+go 1.20
--- main.go
+++ main.go
@@ -6,42 +6,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/fcanmekikoglu/go_experiment/types"
 )
-
-type User struct {
-	ID        string `json:"_id"`
-	Version   string `json:"_v"`
-	CreatedAt string `json:"createdAt"`
-	UpdatedAt string `json:"updatedAt"`
-	Email     string `json:"email"`
-	IsAdmin   bool   `json:"isAdmin"`
-	Name      struct {
-		First string `json:"first"`
-		Last  string `json:"last"`
-	} `json:"name"`
-	Google struct {
-		ID           string `json:"id"`
-		AccessToken  string `json:"accessToken"`
-		RefreshToken string `json:"refreshToken"`
-	} `json:"google"`
-}
-
-type Fact struct {
-	ID        string `json:"_id"`
-	Version   int    `json:"_v"`
-	User      string `json:"user"`
-	CreatedAt string `json:"createdAt"`
-	UpdatedAt string `json:"updatedAt"`
-	Deleted   bool   `json:"deleted"`
-	Source    string `json:"source"`
-	Type      string `json:"type"`
-	Text      string `json:"text"`
-	Status    struct {
-		Verified  bool   `json:"verified"`
-		SentCount int    `json:"sentCount"`
-		Feedback  string `json:"feedback"`
-	} `json:"status"`
-}
 
 func main() {
 	// Make request
@@ -64,7 +31,7 @@ func main() {
 	}
 
 	// Unmarshal facts and extract
-	var facts []Fact
+	var facts []types.Fact
 	json.Unmarshal(responseData, &facts)
 
 	// Iterate over facts
